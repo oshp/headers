e005c748a89e26da8a67e41e7020b4e8c0bbe625
alexandre menezes <alexandre.fmenezes@gmail.com>
2016-08-10 11:11:38 -0300

    Minor changes

--- html/data.php
+++ html/data.php
@@ -1,44 +1,44 @@
 <?php
-function normalizeSpecialCharacters( $str )
+function normalize_special_characters($str)
 {
     # Quotes cleanup
-    $str = ereg_replace( chr(ord("`")), "'", $str );    # `
-    $str = ereg_replace( chr(ord("´")), "'", $str );    # ´
-    $str = ereg_replace( chr(ord("„")), ",", $str );    # „
-    $str = ereg_replace( chr(ord("`")), "'", $str );    # `
-    $str = ereg_replace( chr(ord("´")), "'", $str );    # ´
-    $str = ereg_replace( chr(ord("“")), "\"", $str );   # “
-    $str = ereg_replace( chr(ord("”")), "\"", $str );   # ”
-    $str = ereg_replace( chr(ord("´")), "'", $str );    # ´
-    $str = ereg_replace( chr(147), "\"", $str );        # ´
-    $str = ereg_replace( chr(148), "\"", $str );        # ´
+    $str = ereg_replace(chr(ord("`")), "'", $str);    # `
+    $str = ereg_replace(chr(ord("´")), "'", $str);    # ´
+    $str = ereg_replace(chr(ord("„")), ",", $str);    # „
+    $str = ereg_replace(chr(ord("`")), "'", $str);    # `
+    $str = ereg_replace(chr(ord("´")), "'", $str);    # ´
+    $str = ereg_replace(chr(ord("“")), "\"", $str);   # “
+    $str = ereg_replace(chr(ord("”")), "\"", $str);   # ”
+    $str = ereg_replace(chr(ord("´")), "'", $str);    # ´
+    $str = ereg_replace(chr(147), "\"", $str);        # ´
+    $str = ereg_replace(chr(148), "\"", $str);        # ´
 
-    $unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z',
-                                'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A',
-                                'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C',
-                                'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E',
-                                'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I',
-                                'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
-                                'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
-                                'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y',
-                                'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a',
-                                'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a',
-                                'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
-                                'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i',
-                                'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n',
-                                'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
-                                'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u',
-                                'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
-                                'ÿ'=>'y' );
-    $str = strtr( $str, $unwanted_array );
+    $unwantedArray = array('Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z',
+      'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A',
+      'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C',
+      'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E',
+      'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I',
+      'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
+      'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
+      'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y',
+      'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a',
+      'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a',
+      'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
+      'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i',
+      'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n',
+      'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
+      'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u',
+      'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
+      'ÿ'=>'y' );
+    $str = strtr($str, $unwantedArray);
 
     # Bullets, dashes, and trademarks
-    $str = ereg_replace( chr(149), "&#8226;", $str );    # bullet •
-    $str = ereg_replace( chr(150), "&ndash;", $str );    # en dash
-    $str = ereg_replace( chr(151), "&mdash;", $str );    # em dash
-    $str = ereg_replace( chr(153), "&#8482;", $str );    # trademark
-    $str = ereg_replace( chr(169), "&copy;", $str );     # copyright mark
-    $str = ereg_replace( chr(174), "&reg;", $str );      # registration mark
+    $str = ereg_replace(chr(149), "&#8226;", $str);    # bullet •
+    $str = ereg_replace(chr(150), "&ndash;", $str);    # en dash
+    $str = ereg_replace(chr(151), "&mdash;", $str);    # em dash
+    $str = ereg_replace(chr(153), "&#8482;", $str);    # trademark
+    $str = ereg_replace(chr(169), "&copy;", $str);     # copyright mark
+    $str = ereg_replace(chr(174), "&reg;", $str);      # registration mark
 
     return $str;
 }
@@ -46,7 +46,7 @@ function normalizeSpecialCharacters( $str )
 $con = mysql_connect("localhost","root","password");
 
 if (!$con) {
-  die('Could not connect: ' . mysql_error());
+  die('Could not connect: '. mysql_error());
 }
 
 mysql_select_db("headers", $con);
@@ -62,7 +62,7 @@ $result = mysql_query("SELECT header_value.value AS '". $_GET["header"]
 
 $rows = array();
 while($r = mysql_fetch_array($result)) {
-    $row[0] = "\"" . normalizeSpecialCharacters($r[0]) . "\"";
+    $row[0] = "\"". normalize_special_characters($r[0]) ."\"";
     $row[1] = $r[1];
     array_push($rows,$row);
 }
