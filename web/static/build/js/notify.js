46102fac0479a9a6cc898fabc12f15efa0bbb744
alexandre menezes <alexandre.fmenezes@gmail.com>
2017-06-04 19:39:48 -0300

    added cache support for js files

--- web/static/build/js/notify.js
+++ web/static/build/js/notify.js
@@ -1,1 +1,1 @@
-$(function(){new PNotify({title:"Oh No!",text:"{{ message }}",type:"error",styling:"bootstrap3"})});
+$(function(){new PNotify({title:"Oh No!",text:"This website was not found in our database.",type:"error",styling:"bootstrap3"})});
