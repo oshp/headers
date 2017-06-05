3a7a33c938e9d80396257cd115fc44f4c7e857a0
alexandre menezes <alexandre.fmenezes@gmail.com>
2017-06-05 01:13:55 -0300

    CSP updated

--- web/webui.py
+++ web/webui.py
@@ -129,9 +129,9 @@ def apply_caching(response):
     response.headers["Content-Security-Policy"] = "base-uri 'self'; " \
         "media-src 'none'; " \
         "img-src 'self'; " \
-        "script-src 'self' https://www.google-analytics.com https://ssl.google-analytics.com https://js-agent.newrelic.com https://ajax.cloudflare.com https://cdnjs.cloudflare.com; " \
+        "script-src 'self' http://oshp.bsecteam.com https://www.google-analytics.com https://ssl.google-analytics.com https://js-agent.newrelic.com https://ajax.cloudflare.com https://cdnjs.cloudflare.com; " \
         "font-src 'self' https://cdnjs.cloudflare.com; " \
-        "form-action 'self'"
+        "form-action 'self' http://oshp.bsecteam.com"
     response.headers["X-Frame-Options"] = "DENY"
     response.headers["X-XSS-Protection"] = "1; mode=block; report=https://oshp.bsecteam.com/xssreport"
     response.headers["X-Content-Type-Options"] = "nosniff"
