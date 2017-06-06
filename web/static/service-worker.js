ebfb8c4b7933f78454768065626d315781bc63d1
amenezes <alexandre.fmenezes@gmail.com>
2017-06-05 22:57:54 -0300

    updated PWS

--- web/static/service-worker.js
+++ web/static/service-worker.js
@@ -33,3 +33,12 @@ self.addEventListener('install', function(e) {
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
 });
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
