e70cebbc59d78bca6a3f3e391a655c70b091a68a
amenezes <alexandre.fmenezes@gmail.com>
2017-06-06 13:03:42 -0300

    added support to PWA

--- web/static/service-worker.js
+++ web/static/service-worker.js
@@ -1,3 +1,4 @@
+var dataCacheName = 'oshp-v3.1.0'
 var cacheName = 'oshp-3.1.0';
 var filesToCache = [
   '/summary',
@@ -5,7 +6,7 @@ var filesToCache = [
   '/static/font-awesome/css/font-awesome.min.css',
   '/static/nprogress/nprogress.css',
   '/static/build/css/custom.min.css',
-  '/static/images/owasp_icon.jpg',
+  '/static/images/owasp_icon.png',
   '/static/echarts/dist/echarts.min.js',
   '/static/jquery/dist/jquery.min.js',
   '/static/jquery/dist/jquery.dataTables.min.js',
@@ -14,10 +15,7 @@ var filesToCache = [
   '/static/nprogress/nprogress.min.js',
   '/static/build/js/custom.min.js',
   '/static/build/js/datatable.js',
-  '/static/font-awesome/fonts/fontawesome-webfont.woff2?v=4.6.3',
-  '/static/images/owasp_builders.png',
-  '/static/images/owasp_defenders.png',
-  '/about'
+  '/static/font-awesome/fonts/fontawesome-webfont.woff2?v=4.6.3'
 ];
 
 self.addEventListener('install', function(e) {
@@ -32,6 +30,17 @@ self.addEventListener('install', function(e) {
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e) {
