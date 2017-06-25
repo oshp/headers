6cb92c0542265f9be688c0357d072f025c3e89e6
amenezes <alexandre.fmenezes@gmail.com>
2017-06-24 21:57:40 -0300

    CSP Policy uptaded

--- web/webui.py
+++ web/webui.py
@@ -130,7 +130,7 @@ def page_not_found(e):
 
 @app.after_request
 def apply_caching(response):
-    response.headers["Content-Security-Policy"] = "base-uri 'self'; media-src 'none'; img-src 'self'; font-src 'self'"
+    response.headers["Content-Security-Policy"] = "base-uri 'self'; media-src 'none'"
     response.headers["X-Frame-Options"] = "DENY"
     response.headers["X-XSS-Protection"] = "1; mode=block; report=https://oshp.bsecteam.com/xssreport"
     response.headers["X-Content-Type-Options"] = "nosniff"
