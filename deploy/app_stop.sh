738671386e26bf7e8ca741499977daefdf7bd132
amenezes <alexandre.fmenezes@gmail.com>
2017-06-24 23:36:28 -0300

    updated scripts to auto deploy staging environment

--- deploy/app_stop.sh
+++ deploy/app_stop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd /opt/headers
-docker stack rm oshp
-docker stop $(docker ps -q)
+cd /opt/headers && \
+docker stack rm oshp && \
+docker stop $(docker ps -q) && \
 docker rmi -f $(docker images | grep -i secureheaders | awk '{print $3}')
