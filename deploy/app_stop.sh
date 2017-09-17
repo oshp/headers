20c1d8874f529d288ecda7e5f31cc1a2e714579a
amenezes <alexandre.fmenezes@gmail.com>
2017-09-17 00:16:51 -0300

    minor changes

--- deploy/app_stop.sh
+++ deploy/app_stop.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 cd /opt/headers
 git pull
-#docker stack rm oshp
--- deploy/before_install.sh
+++ deploy/before_install.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-docker rmi -f $(docker images | grep -i none | awk '{print $3}')
+IMAGES_UNTAGGED=$(docker images | grep -i none)
+if [ -n "$IMAGES_UNTAGGED" ]; then
+  docker rmi -f $(docker images | grep -i none | awk '{print $3}')
+fi
