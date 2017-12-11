5c52048516b437fcd300f18cb3bf4cbbc106dbdd
amenezes <alexandre.fmenezes@gmail.com>
2017-12-11 00:14:37 -0200

    sw.js changed

--- web/static/service-worker.js
+++ web/static/service-worker.js
@@ -1,14 +1,6 @@
-var dataCacheName = 'oshp-v3.1.4';
-var cacheName = 'oshp-3.1.4';
+var cacheNames = 'oshp-3.1.4';
 var filesToCache = [
   '/summary',
-  '/total',
-  '/sts',
-  '/pkp',
-  '/xfo',
-  '/xcto',
-  '/csp',
-  '/xxss',
   '/about',
   '/siteinfo',
   '/static/dist/css/bootstrap.min.css',
@@ -30,32 +22,27 @@ var filesToCache = [
 ];
 
 self.addEventListener('install', function(e) {
-  //console.log('[ServiceWorker] Install');
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      //console.log('[ServiceWorker] Caching app shell');
+    caches.open(cacheNames).then(function(cache) {
       return cache.addAll(filesToCache);
     })
   );
 });
 
 self.addEventListener('activate', function(e) {
-  //console.log('[ServiceWorker] Activate');
   e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName && key !== dataCacheName) {
-          //console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
-});
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {}).map(
+          function(cacheName) {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+    );
+  });
 
 self.addEventListener('fetch', function(e) {
-  //console.log('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
