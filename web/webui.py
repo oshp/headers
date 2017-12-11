3a2e02610cd6d30652db87e8cc162027fd2ce0b0
amenezes <alexandre.fmenezes@gmail.com>
2017-12-11 01:32:43 -0200

    script-src changed

--- web/webui.py
+++ web/webui.py
@@ -147,7 +147,7 @@ def apply_caching(response):
     response.headers["X-Frame-Options"] = "DENY"
     response.headers["Content-Security-Policy"] = "default-src https://oshp.bsecteam.com 'self'; " \
         "script-src https://oshp.bsecteam.com https://bam.nr-data.net https://js-agent.newrelic.com " \
-        "https://ssl.google-analytics.com https://ajax.cloudflare.com https://sentry.io https://cdn.ravenjs.com 'self'; " \
+        "https://ssl.google-analytics.com https://www.google-analytics.com https://ajax.cloudflare.com https://sentry.io https://cdn.ravenjs.com 'self'; " \
         "style-src https://oshp.bsecteam.com https://sentry.io 'self' 'unsafe-inline'; " \
         "img-src https://oshp.bsecteam.com https://sentry.io 'self'; " \
         "font-src https://oshp.bsecteam.com 'self'; " \
